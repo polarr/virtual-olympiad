dbd2091cbdd250de794b8385f6fa5dbbdae197c1
1egend <Charles.ran9@gmail.com>
2021-10-17 23:03:15 -0400

    Major Update v0.9.0
    - React replacing Jamstack
    - Sass replacing CSS
    - Complete rewrite of server logic
    - Frontend served over same address
    - Major UI improvements
    - Lobby settings upgrade

---
+++ client/src/App.js
@@ -0,0 +1,79 @@
+import './App.scss';
+import './Menu.scss';
+import './Lobby.scss';
+import './Exam.scss';
+
+import Lobby from './Lobby.js';
+import Exam from './Exam.js';
+import Menu from './Menu.js';
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, {useState, useEffect} from 'react';
+import io from 'socket.io-client';
+
+function App() {
+  const [socket, setSocket] = useState(null);
+  const [page, setPage] = useState(0);
+  
+  useEffect(()=> {
+    const newSocket = io();
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, []);
+
+  useEffect(()=> {
+    if (socket == null){
+      return;
+    }
+    function joinMenu(){
+      setPage(0);
+    }
+    function joinLobby(){
+      setPage(1);
+    }
+    function joinGame(){
+      setPage(2);
+    }
+
+    socket.on("page-menu", joinMenu);
+    socket.on("page-lobby", joinLobby);
+    socket.on("page-exam", joinGame);
+
+    return ()=> {
+      socket.off("page-menu", joinMenu);
+      socket.on("page-lobby", joinLobby);
+      socket.on("page-exam", joinGame);
+    };
+  }, [socket]);
+
+  if (socket){
+    switch(page){
+      case 1:
+        return (
+          <div className="App">
+            <Lobby socket = {socket}/>
+          </div>
+        );
+      case 2:
+        return (
+          <div className="App">
+            <Exam socket = {socket}/>
+          </div>
+        );
+      case 0: default:
+        return (
+          <div className="App">
+            <Menu socket = {socket} />
+          </div>
+        );
+    }
+  }
+
+  return (
+    <div className="App">
+      Not connected to server...
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Menu.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+
+function JoinRoom({name, socket}){
+    let [code, setCode] = useState("");
+    const handleClick = () => {
+        socket.emit('join-room', {name, code});
+    };
+
+    return (
+        <div className = "join-room">
+            <input type="text" className = "code-input" name="code-input" maxLength="4" placeholder="Code" value={code} onInput={e => setCode(e.target.value)}/>
+            <button className = "join-room-button" onClick = {handleClick}>Join Room</button>
+        </div>
+    );
+}
+
+function ModeOption({value, mode, name, ...other}){
+    return <option {...other} value = {value} selected={mode === value ? "selected":""}>{name}</option>
+}
+
+function CreateRoom({name, socket}){
+    let [mode, setMode] = useState(0);
+    const handleClick = () => {
+        socket.emit('create-room', {name, mode});
+    };
+
+    return (
+        <div className = "create-room">
+            <select className = "select-mode" name="select-mode" value={mode} onChange = {e => setMode(e.target.value)}>
+                <ModeOption value = {0} mode = {mode} name = "Normal"/>
+                /** <ModeOption value = {1} mode = {mode} name = "Practice"/>
+                <ModeOption value = {2} mode = {mode} name = "Time Rush"/> **/
+            </select>
+            <button className = "create-room-button" onClick = {handleClick}>Create Room</button>
+        </div>
+    );
+}
+
+function MenuToggle({isActive, name, ...extra}) {
+    return <div {...extra} className = {"selector-option" + (isActive ? " selected":"")}>{name}</div>;
+}
+
+function Menu({socket}) {
+    let [menuToggle, setMenuToggle] = useState(true);
+    let [name, setName] = useState("");
+
+    return (
+        <section className = "menu">
+            <p className = "menu-title"><img className = "menu-logo" src="/logo/vertical_logo.svg" alt="Virtual Olympiad"/></p>
+            <input type="text" className = "name-input" name="name-input" minLength = "1" maxLength="16" placeholder="Enter a name" value={name} onInput={e => setName(e.target.value)}/>
+            <section className = "menu-settings">
+                <div className = "menu-selector">
+                    <MenuToggle isActive = {menuToggle} name = "Join" onClick = {e => setMenuToggle(true)} />
+                    <MenuToggle isActive = {!menuToggle} name = "Create" onClick = {e => setMenuToggle(false)} />
+                </div>
+                <div className = "menu-details">
+                    {menuToggle ? <JoinRoom name = {name} socket = {socket}/>: <CreateRoom name = {name} socket = {socket}/>}
+                </div>
+            </section>
+        </section>
+    );
+}
+
+export default Menu;
